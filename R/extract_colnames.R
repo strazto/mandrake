7a23c7e4158b2d14b4c9a13a27e3800934671708
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-07-06 18:27:18 +1000

    Complete more of implementing column extraction and start to document

--- R/extract_colnames.R
+++ R/extract_colnames.R
@@ -1,7 +1,19 @@
+extract_target_column_names <- function(target, cache) {
+  out <- target %>% drake::readd(character_only = TRUE, cache = cache)
+
+  out %<>% names()
+
+  out
+}
+
 
 #' Extract column names for targets in plan
-#' Maybe allow
-extract_column_names <- function(plan, cache, group = NULL, clusters = NULL, ...) {
+#'
+#' @inheritParams drake::drake_graph_info
+#' @param colname_out the name given to the list-column containing each target's columns
+#' @export
+extract_column_names <- function(plan, cache, group = NULL, clusters = NULL,
+                                 colname_out = "target_column_list", ...) {
   # Handle missingness for grouping data
   if (rlang::is_empty(group)) group <- "target"
   if (rlang::is_empty(clusters)) clusters <- plan %>% dplyr::pull(group) %>% unique()
@@ -19,6 +31,12 @@ extract_column_names <- function(plan, cache, group = NULL, clusters = NULL, ...
     dplyr::group_by(group) %>%
     dplyr::slice_head(n = 1) %>%
     dplyr::ungroup()
+
+  out %<>%
+    dplyr::select(target) %>%
+    dplyr::mutate(!! colname_out := purrr::map(
+      ., extract_target_column_names, cache = cache)
+      )
 
   out
 }
