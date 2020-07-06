f3e64be4ac5d8bdc6e3f5a8021e4574e002a90f3
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-07-06 19:50:35 +1000

    Finish writing extract_column_names

--- R/extract_colnames.R
+++ R/extract_colnames.R
@@ -14,29 +14,38 @@ extract_target_column_names <- function(target, cache) {
 #' @export
 extract_column_names <- function(plan, cache, group = NULL, clusters = NULL,
                                  colname_out = "target_column_list", ...) {
+
   # Handle missingness for grouping data
   if (rlang::is_empty(group)) group <- "target"
   if (rlang::is_empty(clusters)) clusters <- plan %>% dplyr::pull(group) %>% unique()
 
-  out <- plan
+  tmp_col = "temporary_join_col__"
 
-  out %<>%
-    dplyr::mutate_at(
-      group,
-      dplyr::if_else(
-        is.na,
+  plan %<>%
+    dplyr::mutate(
+      !!tmp_col := dplyr::if_else(
+        is.na(),
         target,
         .
-        )) %>%
-    dplyr::group_by(group) %>%
+      ))
+
+  out <- plan
+
+  # Within the group, subset it, if it should cluster
+  out %>%
+    dplyr::group_by(!!rlang::sym(tmp_col)) %>%
     dplyr::slice_head(n = 1) %>%
     dplyr::ungroup()
 
   out %<>%
-    dplyr::select(target) %>%
+    dplyr::select(target, !!group, !!tmp_col) %>%
     dplyr::mutate(!! colname_out := purrr::map(
-      ., extract_target_column_names, cache = cache)
-      )
+      target, extract_target_column_names, cache = cache)
+      ) %>%
+    dplyr::select(!!tmp_col, !!colname_out)
+
+  out %<>%
+    dplyr::right_join(plan, by = tmp_col)
 
   out
 }
