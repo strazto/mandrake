c0a71b74e7cae15319a06fb4503a5ab977f5e042
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-07-08 19:31:49 +1000

    Rename "dir" -> direction, "alias" -> aliases

--- R/roxygen.R
+++ R/roxygen.R
@@ -10,9 +10,9 @@ roxy_tag_parse.roxy_tag_col <- function(x) {
   front_pattern <- glue::glue(
     "{open_pat}{dir_pat}{spc_pat}*{name_pat}{spc_pat}*{alias_pat}",
     open_pat = glue::glue("^{spc_pat}*"), # Match 0 or more whitespace characters from start
-    dir_pat  = "(?:(?<dir>\\[[^[\\]]*?\\]))?", # Optionally match a direction in square brackets [direction]
+    dir_pat  = "(?:(?<direction>\\[[^[\\]]*?\\]))?", # Optionally match a direction in square brackets [direction]
     name_pat = "(?<name>\\w+)", # Match wordy characters
-    alias_pat  = "(?:(?<alias>\\[[^[\\]]*?\\]))?" #Option match a yaml array style list in parentheses
+    alias_pat  = "(?:(?<aliases>\\[[^[\\]]*?\\]))?" #Option match a yaml array style list in parentheses
   )
 
   matched_sig <- gregexpr(front_pattern, x, perl = TRUE)
@@ -39,7 +39,7 @@ roxy_tag_parse.roxy_tag_col <- function(x) {
       match = stringr::str_sub(x, capture.start, capture_end)
       ) %>%
     tidyr::pivot_wider(id_cols = name, names_from = name, values_from = match) %>%
-    dplyr::mutate(dplyr::across(c(dir, alias), ~list(yaml::yaml.load(.))))
+    dplyr::mutate(dplyr::across(c(direction, aliases), ~list(yaml::yaml.load(.))))
 
   matches
 }
