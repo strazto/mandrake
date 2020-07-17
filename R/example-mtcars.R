3a8349be5ecf6d6b3bc93e00dc093a1c7d602df9
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-07-17 21:15:42 +1000

    Add mtcars documentation

---
+++ R/example-mtcars.R
@@ -0,0 +1,44 @@
+#' mtcars dataset
+#'
+#' These cars go vroom!
+#'
+#' @col [out] mpg
+#' Numeric.
+#' `mpg` or "Miles Per Gallon" is how many miles you can drive on
+#' a gallon of fuel.
+#' @col [out] cyl
+#' Integer
+#' Number of cylinders
+#' @col [out] disp
+#' Integer.
+#' I literally don't know what this means
+#' @col [out] hp
+#' Integer
+#' Horsepower is a measure of the number of miniature horses this car
+#' can fit inside itself, which run on a treadmill that make it go fast.
+#' @col [out] drat
+#' Numeric
+#' This is a word you say when something doesn't go the way you hoped.
+#' **Drat!**
+#' @col [out] wt
+#' Numeric
+#' Stands for "What the..." - An exclamation of surprise!
+#'
+#' Or **weight**, maybe.
+#' @col [out] qsec
+#' Numeric.
+#' Number of seconds you need to line up in a queue in order to buy this car.
+#' @col [out] vs
+#' Logical, Does the car ever compete?
+#' @col [out] am
+#' Logical, is this car okay to use in the morning, or should you save it for
+#' after midday?
+#' @col [out] gear
+#' Integer,
+#'
+#' How many different gears transmissions does this car have.
+#' @col [out] carb
+#' Integer, Number of grams of carbohydrates contained in this car.
+#' Typically pretty low, as cars are not particularly nutritious.
+#' @md
+mtcars
