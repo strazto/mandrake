596a61512dcf548f7a3e6ce6b11edac4093bc38f
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-07-17 16:25:02 +1000

    Copy block naming logic from roxygen2's source code

--- R/roxy-roclet-col.R
+++ R/roxy-roclet-col.R
@@ -19,7 +19,10 @@ get_block_data <- function(
   roc, block, env, base_path) {
   `%||%` <- rlang::`%||%`
 
-  topic <- block$object$topic
+  # From:
+  # https://github.com/r-lib/roxygen2/blob/c73def498f29783044fe50865f22c2482f80942d/R/rd.R#L122
+  topic <- block_get_tag(block, "name")$val %||% block$object$topic
+
   pkg <- roxygen2::roxy_meta_get("current_package")
 
   out <- roxygen2::block_get_tags(block, "col")
