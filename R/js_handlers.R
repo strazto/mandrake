cf46c77164d377c7077b1a0a1cfdf8be517862ec
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-07-20 17:29:20 +1000

    Update docstring for embed_event_handler

--- R/js_handlers.R
+++ R/js_handlers.R
@@ -4,6 +4,9 @@
 #' If attached as onSelect behaviour for a visNetwork graph,
 #' will allow clicking of nodes to replace the legend for the graph
 #' with metadata about that node.
+#'
+#' Presently requires JQuery to operate, so may not work when launched from
+#' R session, but does work in pkgdown.
 #' @export
 #' @family js_handlers
 embed_event_handler <- function() {
