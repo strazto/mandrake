515c6b230044afe94732fd9da98a2ecdd2857881
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-08-09 23:33:34 +1000

    Add test stubs for col_roclet

---
+++ tests/testthat/test_col_roclet.R
@@ -0,0 +1,19 @@
+context("Does col roclet correctly process col tags?")
+
+
+test_that("Col roclet correctly handles col tags", {
+  fail("TNI")
+})
+
+test_that("Col roclet correctly handles inheritCol tags", {
+  fail("TNI")
+})
+
+test_that("Col roclet correctly handles no inheritCol tags", {
+  fail("TNI")
+})
+
+test_that("Col roclet correctly handles no col tags", {
+  fail("TNI")
+})
+
