31a5739d9123aa9c73fece34eaa8769dba3432c9
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-08-10 00:33:19 +1000

    Further expand tests

--- tests/testthat/test_col_roclet.R
+++ tests/testthat/test_col_roclet.R
@@ -9,10 +9,6 @@ test_that("Col roclet correctly handles inheritCol tags", {
   fail("TNI")
 })
 
-test_that("Col roclet correctly handles no inheritCol tags", {
-  fail("TNI")
-})
-
 setup_input_no_tags <- function() {
 "
 #' Hello
@@ -21,13 +17,17 @@ foo <- function() 'bar'
 "
 }
 
-test_that("Col roclet correctly handles no col tags", {
+test_that("Col roclet correctly handles no col / inheritCol tags", {
   input <- setup_input_no_tags()
   d <- tempdir()
 
   results <- roxygen2::roc_proc_text(col_roclet(), input)
 
+  expect_true(rlang::is_empty(results) | nrow(results) == 0)
+
   roxygen2::roclet_output(col_roclet(), results, d)
+
+  expect_false(file.exists(file.path(d, "inst/mandrake")))
 
 
 })
