06354b04cfb8496f0d555a82e0d6d1be65d30bcd
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-08-09 20:05:11 +1000

    Update roxygen test

--- inst/test_roxygen.R
+++ inst/test_roxygen.R
@@ -26,3 +26,43 @@ matthews_function2 <- function(df) df
 input_vector <- c(input, input2col)
 
 blocks <- roxygen2::parse_text(input_vector)
+
+
+input_broke <- "
+#' BUM
+#' hey
+#' @col [in, !] broke [i am broke, !]
+my_fun <- function(x) return(x)
+"
+
+topic_broke <- roxygen2::roc_proc_text(roxygen2::rd_roclet(), input_broke)
+
+broke_blocks <- roxygen2::parse_text(input_broke)
+
+test_broke_tag <- roxygen2::roxy_tag("col", " [in] broke [i am broke, !]")
+
+parsed_broke <- roxygen2::roxy_tag_parse(test_broke_tag)
+
+# Test inherit parsing
+
+input_inherit <- "
+#' Hello
+#' @col [in] matthews_col [matts_col]
+#' I love matt's col. It is **Beautiful**.
+#' Check out [mandrake::extract_column_names()]  is_in <- any(x$val$direction %in% dirs_in)
+#'
+#' @inheritCol mandrake [mpg, cyl]
+#' @md
+matthews_function_inherit <- function(df) df
+"
+
+topic <- roxygen2::roc_proc_text(roxygen2::rd_roclet(), input_inherit)
+
+inherit_col_roclet_procd <- roxygen2::roc_proc_text(mandrake::col_roclet(), input_inherit)
+
+block_inherit <- roxygen2::parse_text(input_inherit)
+
+
+my_test_tag <- roxygen2::roxy_tag("inheritCol", " mandrake [mpg]")
+
+roxygen2::roxy_tag_parse(my_test_tag) -> parsed_inherit
