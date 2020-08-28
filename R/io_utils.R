3067d712dcd8b4b60fb7731315896020812c82d2
Matthew Strasiotto <39424834+mstr3336@users.noreply.github.com>
2020-08-28 19:44:35 +1000

    Fix serialization w/ column duplication (#17)
    * When serializing datasets, now only keep distinct entries
    
    * Drop grouping cols in add_entry_to_cache for compat w/ group_modify
    
    * Drop entries whose names are re-defs
    
    * Bump patch version

--- R/io_utils.R
+++ R/io_utils.R
@@ -138,6 +138,8 @@ add_entry_to_cache <- function(entry, keys, lookup_cache = NULL) {
   if (rlang::is_empty(lookup_cache))
     stop("Empty lookup cache given to add_entry_to_cache")
 
+  grouping_cols <- names(keys)
+
   keys %<>% dplyr::pull()
 
   main_key <- keys
@@ -201,17 +203,27 @@ add_entry_to_cache <- function(entry, keys, lookup_cache = NULL) {
 
   keys %<>% handle_previous_defs(entry, lookup_cache)
 
-  # Make the value referencable by the formal name, or any of its
-  # aliases
-  lookup_cache$fill(keys, entry)
-  lookup_cache$fill(keys, entry, namespace = pkg_ns)
-  # Add it to the 1:1 namespace that links formal name to values
-  # (no alias linkage)
-  lookup_cache$duplicate(
-    main_key,
-    main_key,
-    namespace_src = src_namespace,
-    namespace_dest = dest_namespace
+  entry %<>% dplyr::filter(name %in% keys)
+
+  # Only bother filling the cache if there's something to fill
+  if (!rlang::is_empty(keys) & nrow(entry) > 0) {
+    # Make the value referencable by the formal name, or any of its
+    # aliases
+    lookup_cache$fill(keys, entry)
+    lookup_cache$fill(keys, entry, namespace = pkg_ns)
+    # Add it to the 1:1 namespace that links formal name to values
+    # (no alias linkage)
+    lookup_cache$duplicate(
+      main_key,
+      main_key,
+      namespace_src = src_namespace,
+      namespace_dest = dest_namespace
     )
-  invisible(NULL)
+  }
+
+  # Drop these before return to ensure functionality
+  # with group_modify
+  entry %<>% dplyr::select(-c(grouping_cols))
+
+  invisible(entry)
 }
