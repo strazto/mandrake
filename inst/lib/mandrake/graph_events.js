e220773a84aff573cf165129964a274b509d633f
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2021-04-07 19:04:15 +1000

    add template handler function

--- inst/lib/mandrake/graph_events.js
+++ inst/lib/mandrake/graph_events.js
@@ -27,3 +27,22 @@ const alert_handler = function(props) {
     '=============== '
   );
 }
+
+
+const dummy_template_handler = function(props) {
+  container = this.body.container;
+  legend = $(container).parent().children("[id*='legendhtmlwidget']")[0];
+  node = this.body.data.nodes.get(props.nodes[0]);
+  $(legend).css({
+    'font-size': '10px',
+    'width' : '30%',
+    'overflow-y' : 'scroll'
+  });
+
+  $(container).css('width', '70%');
+
+  //legend.innerHTML = DOMPurify.sanitize(node.on_select_col);
+
+  $(legend).find('pre').css('font-size', 'inherit');
+
+}
