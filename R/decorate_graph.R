1175ff866757c7292ae5cd25df9fc710e7a3c2c2
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2021-04-10 18:42:23 +1000

    make more space for command code
    This is achieved by decreasing indent when reflowing before linking /
    highlighting, as well as reducing padding & text size in the html

--- R/decorate_graph.R
+++ R/decorate_graph.R
@@ -157,7 +157,10 @@ enrich_docstrings <- function(docstrings) {
 highlight_single_command <- function(x) {
   x %<>%
     rlang::expr_deparse(width = Inf) %>%
-    styler::style_text(scope = "line_breaks") %>%
+    styler::style_text(
+      scope = "line_breaks",
+      indent_by = 1
+      ) %>%
     paste(sep = "\n", collapse = "\n") %>%
     downlit::highlight(pre_class = "downlit")
   x
